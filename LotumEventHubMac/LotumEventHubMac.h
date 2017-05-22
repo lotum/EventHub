f85ea41aed06a0b58e26fe89ba20fe9888daeb83
dennis <becker@lotum.de>
2017-05-22 09:46:21 +0200

    Add target for macOS development

---
+++ LotumEventHubMac/LotumEventHubMac.h
@@ -0,0 +1,19 @@
+//
+//  LotumEventHubMac.h
+//  LotumEventHubMac
+//
+//  Created by Dennis Becker on 17.05.17.
+//  Copyright © 2017 LOTUM GmbH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LotumEventHubMac.
+FOUNDATION_EXPORT double LotumEventHubMacVersionNumber;
+
+//! Project version string for LotumEventHubMac.
+FOUNDATION_EXPORT const unsigned char LotumEventHubMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LotumEventHubMac/PublicHeader.h>
+
+
