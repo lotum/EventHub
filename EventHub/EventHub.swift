24a2c5061b147d7e6c43d6579dc2805e52632658
martinreichard <reichard@lotum.de>
2017-11-16 12:03:16 +0100

    Allow action to listen to one of several events

--- EventHub/EventHub.swift
+++ EventHub/EventHub.swift
@@ -12,7 +12,7 @@ import Foundation
 
 
 /** 
- This class is an pub/sub implementation. You can register for events.
+ This class is a pub/sub implementation. You can register for events.
  And you can emit events on specific queues. Threadsafe.
  */
 public final class EventHub<EventT: Hashable, PayloadT> {
@@ -26,10 +26,18 @@ public final class EventHub<EventT: Hashable, PayloadT> {
         return on(event, lifetime: .always, action: action)
     }
     
-    
     @discardableResult
     public func once(_ event: EventT, action: @escaping (PayloadT)->Void) -> Disposable {
         return on(event, lifetime: .once, action: action)
+    }
+    
+    public func on(oneOf events: Set<EventT>, action: @escaping (PayloadT)->Void) -> Disposable {
+        return on(oneOf: events, lifetime: .always, action: action)
+    }
+
+    @discardableResult
+    public func once(oneOf events: Set<EventT>, action: @escaping (PayloadT)->Void) -> Disposable {
+        return on(oneOf: events, lifetime: .once, action: action)
     }
     
     public func removeAllListeners(forEvent: EventT? = nil) {
@@ -37,8 +45,17 @@ public final class EventHub<EventT: Hashable, PayloadT> {
         defer { lock.unlock() }
         if let event = forEvent {
             queuedActions[event] = nil
+            oneOfActions = oneOfActions.flatMap { listener in
+                var newListener = listener
+                newListener.events.remove(event)
+                if newListener.events.isEmpty {
+                    return nil
+                }
+                return newListener
+            }
         } else {
             queuedActions = [:]
+            oneOfActions = []
         }
     }
     
@@ -46,15 +63,23 @@ public final class EventHub<EventT: Hashable, PayloadT> {
         lock.lock()
         defer { lock.unlock() }
         if let event = forEvent {
-            return queuedActions[event]?.count ?? 0
+            let queuedActionsCount = queuedActions[event]?.count ?? 0
+            var oneOfCount = 0
+            for listener in oneOfActions {
+                if listener.events.contains(event) {
+                    oneOfCount += 1
+                }
+            }
+            return queuedActionsCount + oneOfCount
         } else {
-            return queuedActions.values.flatMap { $0 }.count
+            let queuedActionsCount = queuedActions.values.flatMap { $0 }.count
+            return queuedActionsCount + oneOfActions.count
         }
     }
     
     /**
      The event listeners registered for the given event get fired. If you don't specify a queue the listeners get
-     fired on the current queue synchronously. Otherwise they are called asynchonously on the given queue.
+     fired on the current queue synchronously. Otherwise they are called asynchronously on the given queue.
      
      The payload block only evaluate if there are listeners.
     */
@@ -62,8 +87,15 @@ public final class EventHub<EventT: Hashable, PayloadT> {
                      on queue: DispatchQueue? = nil,
                      with value: @autoclosure ()->PayloadT) {
         lock.lock()
-        let actionsToExecute = queuedActions[event] ?? []
-        self.queuedActions[event] = actionsToExecute.flatMap { $0.reduce() }
+        var actionsToExecute = queuedActions[event] ?? []
+        let oneOfActionsToExecute = oneOfActions
+            .filter { $0.events.contains(event) }
+            .map { $0.action }
+        actionsToExecute += oneOfActionsToExecute
+        
+        queuedActions[event] = actionsToExecute.flatMap { $0.reduce() }
+        oneOfActions = oneOfActions.filter { $0.action.runTime == .always || !$0.events.contains(event) }
+        
         lock.unlock()
         //run blocks after filtering the actions, so you can emit from within block()
         //without infinite recursion when emitting the same event although action lifetime is once
@@ -76,6 +108,8 @@ public final class EventHub<EventT: Hashable, PayloadT> {
     //MARK: Private
     
     private var queuedActions: [EventT:[Action<PayloadT>]] = [:]
+    private var oneOfActions: [(action: Action<PayloadT>, events: Set<EventT>)] = []
+    
     private let lock = NSLock()
     
     private func on(_ event: EventT,
@@ -92,6 +126,27 @@ public final class EventHub<EventT: Hashable, PayloadT> {
         }
     }
     
+    private func on(oneOf events: Set<EventT>,
+                    lifetime: ActionLifetime,
+                    action: @escaping (PayloadT)->Void) -> Disposable
+    {
+        if events.count == 0 {
+            return EventDisposable {}
+        }
+        if events.count == 1 {
+            return on(events.first!, lifetime: lifetime, action: action)
+        }
+        
+        lock.lock()
+        let act = Action(runTime: lifetime, action: action)
+        oneOfActions.append((action: act, events: events))
+        lock.unlock()
+        return EventDisposable { [weak self, weak act] in
+            guard let act = act else { return }
+            self?.removeOneOfListener(with: act)
+        }
+    }
+    
     private func removeListener(with toRemove: Action<PayloadT>, forEvent event: EventT) {
         lock.lock()
         defer { lock.unlock() }
@@ -99,6 +154,12 @@ public final class EventHub<EventT: Hashable, PayloadT> {
             let filteredActions = allActionsForEvent.filter { $0 !== toRemove }
             queuedActions[event] = filteredActions
         }
+    }
+    
+    private func removeOneOfListener(with toRemove: Action<PayloadT>) {
+        lock.lock()
+        defer { lock.unlock() }
+        oneOfActions = oneOfActions.filter { $0.action !== toRemove }
     }
 }
 
@@ -161,7 +222,7 @@ private final class Action<PayloadT> {
     }
     
     func reduce() -> Action? {
-        switch self.runTime {
+        switch runTime {
         case .always:
             return self
         case .once:
--- EventHubTests/EventHubTests.swift
+++ EventHubTests/EventHubTests.swift
@@ -25,9 +25,9 @@ class EventHubTest: XCTestCase {
     }
     
     func testMutlipleEmit() {
-        let e1 = expectation(description: "test1")
-        let e2 = expectation(description: "test2")
-        let e3 = expectation(description: "test3")
+        let e1 = expectation(description: "expectation1")
+        let e2 = expectation(description: "expectation2")
+        let e3 = expectation(description: "expectation3")
         
         let hub = EventHub<String, Void>()
         hub.once("test1") {
@@ -90,6 +90,33 @@ class EventHubTest: XCTestCase {
         waitForExpectations(timeout: 1.5, handler: nil)
     }
     
+    func test_remove_one_of_listener() {
+        let e1 = expectation(description: "one")
+        let e2 = expectation(description: "two")
+        
+        let hub = EventHub<String, Void>()
+        
+        hub.once("event1") {
+            e1.fulfill()
+        }
+        
+        hub.once("event2") {
+            e2.fulfill()
+        }
+        
+        let disposer = hub.on(oneOf: ["event1", "event2"]) {
+            XCTFail()
+        }
+        
+        XCTAssertEqual(hub.numberOfListeners(), 3)
+        disposer.dispose()
+        XCTAssertEqual(hub.numberOfListeners(), 2)
+        hub.emit("event1")
+        hub.emit("event2")
+        
+        waitForExpectations(timeout: 1.5, handler: nil)
+    }
+    
     func test_schedule_on_queue() {
         let e = expectation(description: "on_queue")
         
@@ -109,5 +136,109 @@ class EventHubTest: XCTestCase {
         waitForExpectations(timeout: 1.5, handler: nil)
     }
     
+    func test_number_of_listeners() {
+        let hub = EventHub<String, Void>()
+        
+        XCTAssertEqual(hub.numberOfListeners(), 0)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 0)
+        
+        hub.once("test1") {}
+        XCTAssertEqual(hub.numberOfListeners(), 1)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 1)
+        
+        hub.on("test2") {}
+        XCTAssertEqual(hub.numberOfListeners(), 2)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 1)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test2"), 1)
+        
+        hub.on("test1") {}
+        XCTAssertEqual(hub.numberOfListeners(), 3)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 2)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test2"), 1)
+        
+        hub.once(oneOf: ["test1", "test2"]) {}
+        XCTAssertEqual(hub.numberOfListeners(), 4)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 3)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test2"), 2)
+        
+        hub.on(oneOf: ["test1", "test2"]) {}
+        XCTAssertEqual(hub.numberOfListeners(), 5)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 4)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test2"), 3)
+        
+        hub.removeAllListeners(forEvent: "test1")
+        XCTAssertEqual(hub.numberOfListeners(), 3)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 0)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test2"), 3)
+        
+        hub.removeAllListeners()
+        XCTAssertEqual(hub.numberOfListeners(), 0)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test1"), 0)
+        XCTAssertEqual(hub.numberOfListeners(forEvent: "test2"), 0)
+    }
     
+    func test_once_one_of() {
+        let e1 = expectation(description: "one")
+        let e2 = expectation(description: "two")
+        
+        let hub = EventHub<String, Void>()
+        hub.once(oneOf: ["test1", "test2"]) {
+            e1.fulfill()
+        }
+
+        hub.emit("test2")
+        hub.emit("test2")
+        hub.emit("test3")
+        
+        hub.once(oneOf: ["test1", "test2"]) {
+            e2.fulfill()
+        }
+        
+        hub.emit("test1")
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func test_on_one_of() {
+        let es = [expectation(description: "one"), expectation(description: "two")]
+        
+        let hub = EventHub<String, Void>()
+        var index = 0
+        hub.on(oneOf: ["test1", "test2"]) {
+            es[index].fulfill()
+            index += 1
+        }
+        
+        hub.emit("test1")
+        hub.emit("test2")
+        hub.emit("test3")
+
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
+    
+    func test_one_of_with_zero_events() {
+        let hub = EventHub<String, Void>()
+        
+        let disposer = hub.once(oneOf: []) {
+            XCTFail()
+        }
+        
+        hub.emit("event")
+        XCTAssertEqual(hub.numberOfListeners(), 0)
+        disposer.dispose()
+    }
+    
+    func test_one_of_with_single_event() {
+        let e = expectation(description: "expectation")
+        
+        let hub = EventHub<String, Void>()
+        
+        hub.once(oneOf: ["event"]) {
+            e.fulfill()
+        }
+        
+        hub.emit("event")
+        
+        waitForExpectations(timeout: 0.1, handler: nil)
+    }
 }
